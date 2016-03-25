bab34fa2d8b699aff022220a541a5c49110de2d6
Dustin Schaerer <dustinschaerer@gmail.com>
2016-03-25 13:52:16 -0700

    Get static pages and posts working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :posts
-  root to: 'users#index'
+  root to: 'static_pages#home'
+
   devise_for :users
+  authenticated :user do
+    root :to => 'static_pages#home', :as => "authenticated_root"
+  end
+
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :posts
 
 end
