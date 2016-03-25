62a59fc358358f89fcdd58f81e3129ecdc69a08b
Dustin Schaerer <dustinschaerer@gmail.com>
2016-03-24 20:09:25 -0700

    Work on site layout and styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :posts
   root to: 'users#index'
   devise_for :users
 
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,4 +1,9 @@
 FactoryGirl.define do
+  factory :post do
+    title "MyString"
+    content "MyText"
+    user nil
+  end
   factory :user do
     
   end
